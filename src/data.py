a311c70c7c7812ef0a436696ae9eaf41cd402470
HenriMeyer <meyer_henri.1@web.de>
2024-10-16 17:29:56 +0200

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,44 +1,90 @@
 import pandas as pd
 
-def get_data(filename):
+
+
+# Get data and input is filename of the source, don't forget '.csv'
+def read_SMARD(filename):
+    # Path -> move up a directory
     path = "../data/" + filename
     
+    # Try block
     try:
-        # CSV-Datei einlesen
-        df = pd.read_csv(path, sep=';', decimal=',', na_values=['-'])
+        # Read file
+        df = pd.read_csv(
+            path,
+            sep=';',
+            decimal=',',
+            thousands='.',
+            na_values=['-'],
+            parse_dates=[0,1],
+            dayfirst=True
+        )
         
-        # Entferne Punkte aus den Zahlen (ersetze ',' mit '.')
-        df.replace({r'\.': '', ',': '.'}, regex=True, inplace=True)
+        # Change names
+        df = df.rename(
+        columns={
+            'Biomasse [MWh] Originalauflösungen': 'Biomasse',
+            'Wasserkraft [MWh] Originalauflösungen' : 'Wasserkraft',
+            'Wind Offshore [MWh] Originalauflösungen':'Wind Offshore',
+            'Wind Onshore [MWh] Originalauflösungen':'Wind Onshore',
+            'Photovoltaik [MWh] Originalauflösungen':'Photovoltaik',
+            'Sonstige Erneuerbare [MWh] Originalauflösungen':'Sonstige Erneuerbare',
+            'Kernenergie [MWh] Originalauflösungen':'Kernenergie',
+            'Braunkohle [MWh] Originalauflösungen':'Braunkohle',
+            'Steinkohle [MWh] Originalauflösungen':'Steinkohle',
+            'Erdgas [MWh] Originalauflösungen':'Erdgas',
+            'Pumpspeicher [MWh] Originalauflösungen':'Pumpspeicher',
+            'Sonstige Konventionelle [MWh] Originalauflösungen':'Sonstige Konventionelle'
+            }
+        )
+        return df
         
-        # Konvertiere die relevanten Spalten in numerische Werte und ignoriere Fehler
-        numeric_columns = df.columns[2:]  # Ab der 3. Spalte beginnen die Zahlen
-        df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric, errors='coerce')
-        
-        # Summiere die Werte für jede Spalte
-        column_sums = df[numeric_columns].sum()
-        
-        total_sum = column_sums.sum()
+        # # Remove dots and exchange ',' with '.'
+        # df.iloc[:, 2:] = df.iloc[:, 2:].replace({r'\.': '', ',': '.'}, regex=True)
+        # df[2:].replace({r'\.': '', ',': '.'}, regex=True, inplace=True)
         
         
-        renewable_sum = 0
-        # Erneuerbare
-        for i in range(7):
-            renewable_sum += column_sums[i]
-        renewable_sum+=column_sums[10]
+        # # Konvertiere die relevanten Spalten in numerische Werte und ignoriere Fehler
+        # numeric_columns = df.columns[2:]  # Ab der 3. Spalte beginnen die Zahlen
+        # df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric, errors='coerce')
         
-        for i in range(12):
-            print("Die Summe der " + str(i) + ". Spalte: " + str(column_sums[i]))
         
-        print("Summe Gesamt: " + str(total_sum))
-        print("Summe der Erneruabren: " + str(renewable_sum))
-        print("Anteil der Erneubaren: " + str(renewable_sum/total_sum*100)[:5] + "%")
+        # # Setup for data
+        # date_time = df.columns[2:]
+        # renewable = df.iloc[:, list(range(2, 7)) + [11]]
+        # konventional = df.iloc[:, list(range(9, 11)) + [13]]
+        # print(date_time)
         
-        # Ausgabe der Summen für jede Spalte
+        
+        # column_sums = df[numeric_columns].sum()
+        
+        # total_sum = column_sums.sum()
+        # # timestamps = df.iloc[:,0]
+        # # # print(timestamps.iloc[:,0])
+        
+        
+        # renewable_sum = 0
+        # # Erneuerbare
+        # for i in range(7):
+        #     renewable_sum += column_sums[i]
+        # renewable_sum+=column_sums[11]
+        
+        # for i in range(11):
+        #     print("Die Summe der " + str(i) + ". Spalte: " + str(column_sums[i]))
+        
+        # print("Summe Gesamt: " + str(total_sum))
+        # print("Summe der Erneruabren: " + str(renewable_sum))
+        # print("Anteil der Erneubaren: " + str(renewable_sum/total_sum*100)[:5] + "%")
+        # print(df.iloc[:, 0])
+        
+        # # Ausgabe der Summen für jede Spalte
         # print("Summen der Spalten:")
         # print(column_sums)
     
+    # Error handling
     except FileNotFoundError:
-        print("File has not been found.")
+        print(f"File '{filename}' has not been found at path: {path}")
+        
         
 if __name__ == "__main__":
-    get_data("Realisierte_Erzeugung_202410050000_202410160000_Viertelstunde.csv")
+    read_SMARD("Realisierte_Erzeugung_202410050000_202410160000_Viertelstunde.csv")
