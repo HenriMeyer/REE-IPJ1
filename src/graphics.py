aa7cd712905d454a7159c6cec58f99b370a7caf1
Leif <llj.bremer@gmail.com>
2024-10-20 08:39:23 +0200

    generalized plotWeekdayHeatmap function

--- src/graphics.py
+++ src/graphics.py
@@ -2,15 +2,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def plotWeekdayHeatmap(df, title = 'first Try', colName = 'Photovoltaik'):
 
-    heatmap_data = df.pivot_table(index='Tag_von', columns='Uhrzeit_von', values=colName, aggfunc=np.mean) # Leistungsmittelwerte!
+#df: data, 
+# title: headline, 
+# colName: name of column with to be displayed data,
+# indexY: Time on Y-axis
+# indexX: Time on X-axis
+
+def plotWeekdayHeatmap(df, title, colName, indexY, indexX):
+
+    heatmap_data = df.pivot_table(index = indexY, columns = indexX , values=colName, aggfunc=np.mean) # Leistungsmittelwerte!
 
     plt.figure(figsize=(20, 8))
     sns.heatmap(heatmap_data, cmap='coolwarm', annot=False, linewidths=.8, cbar=True, xticklabels=1)
 
     plt.title(title)
-    plt.xlabel('Viertelstunde')
-    plt.ylabel('Wochentag')
+    plt.xlabel(indexX)
+    plt.ylabel(indexY)
 
     plt.show()  
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import graphics
 def main():
     df = data.read_SMARD("Realisierte_Erzeugung_202410050000_202410160000_Viertelstunde.csv")
     print(df)
-    graphics.plotWeekdayHeatmap(df)
+    graphics.plotWeekdayHeatmap(df, "Heatmap", "Photovoltaik", "Tag_von", "Uhrzeit_von")
 
 if __name__ == "__main__":
     main()
