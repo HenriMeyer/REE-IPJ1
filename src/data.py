4184da849772f4847eaa9249b53b75cb9af375bc
HenriMeyer <meyer_henri.1@web.de>
2024-10-28 21:01:07 +0100

    Read new file and new time column

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # Get data and input is filename of the source, don't forget '.csv'
-def read_SMARD(filename):
+def read_SMARD(filename, generation:bool = True):
     # Path -> move up a directory
     path = "../data/" + filename
     
@@ -20,65 +20,80 @@ def read_SMARD(filename):
             dayfirst=True
         )
         
-        # Change names
-        df = df.rename(
-        columns={
-            'Biomasse [MWh] Originalauflösungen': 'Biomasse',
-            'Wasserkraft [MWh] Originalauflösungen' : 'Wasserkraft',
-            'Wind Offshore [MWh] Originalauflösungen':'Wind Offshore',
-            'Wind Onshore [MWh] Originalauflösungen':'Wind Onshore',
-            'Photovoltaik [MWh] Originalauflösungen':'Photovoltaik',
-            'Sonstige Erneuerbare [MWh] Originalauflösungen':'Sonstige Erneuerbare',
-            'Kernenergie [MWh] Originalauflösungen':'Kernenergie',
-            'Braunkohle [MWh] Originalauflösungen':'Braunkohle',
-            'Steinkohle [MWh] Originalauflösungen':'Steinkohle',
-            'Erdgas [MWh] Originalauflösungen':'Erdgas',
-            'Pumpspeicher [MWh] Originalauflösungen':'Pumpspeicher',
-            'Sonstige Konventionelle [MWh] Originalauflösungen':'Sonstige Konventionelle'
-            }
-        )
-
-        # Add further information to dataframe
+        # Format time
         df = formatTime(df)
-        df = addDataInformation(df)
-        df = addPercantageRenewable(df)
         
-        # Reorder the columns
-        new_order = [
-            'Jahr', 'Monat', 'Tag', 'Uhrzeit',
-            'Biomasse', 'Wasserkraft', 'Wind Offshore', 'Wind Onshore',
-            'Photovoltaik', 'Sonstige Erneuerbare', 'Pumpspeicher',
-            'Kernenergie', 'Braunkohle', 'Steinkohle', 'Erdgas', 'Pumpspeicher',
-            'Sonstige Konventionelle','Erneuerbar','Anteil Erneuerbar [%]', 'Total', 'Residual'
-        ]
-        df = df[new_order]
+        if(generation):
+            # Change names
+            df = df.rename(
+            columns={
+                'Biomasse [MWh] Originalauflösungen': 'Biomasse',
+                'Wasserkraft [MWh] Originalauflösungen' : 'Wasserkraft',
+                'Wind Offshore [MWh] Originalauflösungen':'Wind Offshore',
+                'Wind Onshore [MWh] Originalauflösungen':'Wind Onshore',
+                'Photovoltaik [MWh] Originalauflösungen':'Photovoltaik',
+                'Sonstige Erneuerbare [MWh] Originalauflösungen':'Sonstige Erneuerbare',
+                'Kernenergie [MWh] Originalauflösungen':'Kernenergie',
+                'Braunkohle [MWh] Originalauflösungen':'Braunkohle',
+                'Steinkohle [MWh] Originalauflösungen':'Steinkohle',
+                'Erdgas [MWh] Originalauflösungen':'Erdgas',
+                'Pumpspeicher [MWh] Originalauflösungen':'Pumpspeicher',
+                'Sonstige Konventionelle [MWh] Originalauflösungen':'Sonstige Konventionelle'
+                }
+            )
+            
+            # Add further information to dataframe
+            df = addDataInformation(df)
+            df = addPercantageRenewable(df)
         
+            # Reorder the columns
+            new_order = [
+                'Jahr', 'Monat', 'Tag', 'Uhrzeit', 'Monat Tag',
+                'Biomasse', 'Wasserkraft', 'Wind Offshore', 'Wind Onshore',
+                'Photovoltaik', 'Sonstige Erneuerbare', 'Pumpspeicher',
+                'Kernenergie', 'Braunkohle', 'Steinkohle', 'Erdgas', 'Pumpspeicher',
+                'Sonstige Konventionelle','Erneuerbar','Anteil Erneuerbar [%]', 'Total', 'Residual'
+            ]
+            df = df[new_order]
+        else:
+            df = df.rename(
+            columns={'Gesamt (Netzlast) [MWh] Originalauflösungen': 'Gesamt',
+                     'Residuallast [MWh] Originalauflösungen': 'Residuallast',
+                     'Pumpspeicher [MWh] Originalauflösungen': 'Pumpspeicher'
+                }
+            )
+            new_order = ['Jahr', 'Monat', 'Tag', 'Uhrzeit', 'Monat Tag', 'Gesamt', 'Residuallast', 'Pumpspeicher']
+            df = df[new_order]
         return df
         
     # Error handling
     except FileNotFoundError:
         print(f"File '{filename}' has not been found at path: {path}")
 
-# Extract time components and reorder
+
+# Add further information
 def formatTime(df):
     # Extract year, month, day, hour, minute from 'Datum von'
     df['Jahr'] = df['Datum von'].dt.year
     df['Monat'] = df['Datum von'].dt.month
     df['Tag'] = df['Datum von'].dt.day
     df['Uhrzeit'] = df['Datum von'].dt.time
+    df['Monat Tag'] = df['Datum von'].dt.strftime('%m %d')
     
     # Remove the original date columns
     df = df.drop(columns=['Datum von', 'Datum bis'])
-
-    # Return the updated DataFrame
     return df
 
-#-------------------------------------------------------------------------------------
-# Monat und Tag in df
-# Andere Datei einlesen
-# 
-# 
-#-------------------------------------------------------------------------------------
+def addDataInformation(df):
+    df['Erneuerbar'] = df.loc[:,['Biomasse','Wasserkraft','Wind Offshore','Wind Onshore','Photovoltaik','Sonstige Erneuerbare','Pumpspeicher']].sum(axis=1)
+    df['Total'] = df.loc[:,'Biomasse':'Sonstige Konventionelle'].sum(axis=1)
+    df['Residual'] = df['Total']- df['Erneuerbar']
+    return df
+
+def addPercantageRenewable(df):
+    df['Anteil Erneuerbar [%]'] = (df.loc[:,['Biomasse','Wasserkraft','Wind Offshore','Wind Onshore','Photovoltaik','Sonstige Erneuerbare','Pumpspeicher']].sum(axis=1)/df.loc[:,'Biomasse':'Sonstige Konventionelle'].sum(axis=1)*100).round(2)
+    return df
+
 
 # Total functions
 def total_sum(df):
@@ -130,20 +145,8 @@ def countPercentageRenewableExclude(df):
     
     return vector.tolist()
 
-# Add further information to dataframe
-def addPercantageRenewable(df):
-    df['Anteil Erneuerbar [%]'] = (df.loc[:,['Biomasse','Wasserkraft','Wind Offshore','Wind Onshore','Photovoltaik','Sonstige Erneuerbare','Pumpspeicher']].sum(axis=1)/df.loc[:,'Biomasse':'Sonstige Konventionelle'].sum(axis=1)*100).round(2)
-    return df
 
-def addDataInformation(df):
-    df['Erneuerbar'] = df.loc[:,['Biomasse','Wasserkraft','Wind Offshore','Wind Onshore','Photovoltaik','Sonstige Erneuerbare','Pumpspeicher']].sum(axis=1)
-    df['Total'] = df.loc[:,'Biomasse':'Sonstige Konventionelle'].sum(axis=1)
-    df['Residual'] = df['Total']- df['Erneuerbar']
-    return df
-
-
-# For testing: "Realisierte_Erzeugung_202101010000_202201010000_Viertelstunde.csv"
 if __name__ == "__main__":
-    df = read_SMARD("Realisierte_Erzeugung_202101010000_202201010000_Viertelstunde.csv")
+    # df = read_SMARD("Realisierte_Erzeugung_202101010000_202201010000_Viertelstunde.csv")
+    df = read_SMARD("Realisierter_Stromverbrauch_202101010000_202201010000_Viertelstunde.csv", False)
     print(df)
-    print(countPercentageRenewable(df))
