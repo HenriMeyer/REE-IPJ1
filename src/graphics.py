f5b2f00672ecb0ec5d2e23b26999a32d9c77151a
Leif <llj.bremer@gmail.com>
2024-11-11 20:28:39 +0100

    cleaned code, added comments

--- src/graphics.py
+++ src/graphics.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import pandas as pd
+
+
+#functions for plotting
 
 
 #df: data, 
@@ -8,8 +12,9 @@ import numpy as np
 # colName: name of column with to be displayed data,
 # indexY: Time on Y-axis
 # indexX: Time on X-axis
-
-def plotHeatmap(df, colName, indexY, indexX, filename: str):
+def plotHeatmap(df: pd.DataFrame , colName, indexY, indexX, filename: str):
+    
+    print(df)
     
     path = "../data/" + filename + ".png"
     heatmap_data = df.pivot_table(index = indexY, columns = indexX , values=colName, aggfunc=np.mean) # Leistungsmittelwerte!
@@ -20,31 +25,12 @@ def plotHeatmap(df, colName, indexY, indexX, filename: str):
     plt.title(filename)
     plt.xlabel(indexX)
     plt.ylabel(indexY)
-    plt.savefig(path, format='png', dpi=300, bbox_inches='tight')
+    plt.savefig(path, format='png', dpi=300)
     
     plt.show()  
 
-def plotHistogramPercent(vec, filename: str):
-    
+def plotHistogramPercent(df, filename: str):
     path = "../data/" + filename + ".png"
-    x_labels = [f"{(i+1)*10}%" for i in range(10)]
-
-    plt.figure(figsize=(10, 6))
-    bars = plt.bar(x_labels, vec[1:], color='skyblue')
-    plt.xlabel('Anteil Erneuerbar [%]')
-    plt.ylabel('Anzahl an Viertelstunden')
-    plt.title( filename)
-
-    for bar in bars:
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width() / 2, yval + 2, yval, ha='center', va='bottom')
-    plt.savefig(path, format='png', dpi=300, bbox_inches='tight')
-    
-    plt.show()
-
-def plotHistogramPercent2(df, filename: str):
-    path = "../data/" + filename + ".png"
-
 
     # Histogram erstellen und die x-Achse als Prozentwerte anzeigen
     plt.figure(figsize=(10, 6))
@@ -54,157 +40,108 @@ def plotHistogramPercent2(df, filename: str):
     plt.title(filename)
 
     # Speichern und anzeigen
-    plt.savefig(path, format='png', dpi=300, bbox_inches='tight')
+    plt.savefig(path, format='png', dpi=300)
     plt.show()
 
-def plotHistogramErzeuger(df, filename: str):
+#plots collumnchart of renewable energyproducers
+def plot_balk_rene(df, filename: str):
 
-    path = "../data/" + filename + ".png"
     erzeuger_spalten = [
         'Biomasse', 'Wasserkraft', 'Wind Offshore', 'Wind Onshore',
         'Photovoltaik', 'Sonstige Erneuerbare'
     ]
-    
-
     summen = df[erzeuger_spalten].sum()/(1e+6)
-    
+    ylabel = 'Energie in TWh'
+    xlabel = 'Erzeuger'
+    plotBalken(summen, ylabel, xlabel, filename)
 
-    plt.figure(figsize=(10, 6))
-    summen.plot(kind='bar', color='skyblue')
-    plt.title(filename)
-    plt.ylabel('Energie in TWh')
-    plt.xlabel('Erzeuger')
-    plt.xticks(rotation=10)
 
-    for i, value in enumerate(summen):
-        plt.text(i, value + 1, f'{value:.2f}TWh', ha='center', va='bottom')
-
-    plt.savefig(path, format='png', dpi=300, bbox_inches='tight')
-    plt.show()
-
-def plotBalken(df, filename: str):
-
-    path = "../data/" + filename + ".png"
+#plots columnchart of all energyproducers
+def plot_balk_all(df, filename: str):
     erzeuger_spalten = [
         'Biomasse', 'Wasserkraft', 'Wind Offshore', 
         'Wind Onshore', 'Photovoltaik', 'Sonstige Erneuerbare',
         'Kernenergie', 'Braunkohle', 'Steinkohle', 'Erdgas', 
         'Pumpspeicher', 'Sonstige Konventionelle'
     ]
-    
-
     summen = df[erzeuger_spalten].sum()/(1e+6)
-    
+    ylabel = 'Energieerzeugung in TWh'
+    xlabel = 'Erzeuger'
+    plotBalken(summen, ylabel, xlabel, filename)
 
-    plt.figure(figsize=(10, 6))
-    summen.plot(kind='bar', color='skyblue')
-    plt.title(filename)
-    plt.ylabel('Energieerzeugung in TWh')
-    plt.xlabel('Erzeuger')
-    plt.xticks(rotation=15)
-
-    for i, value in enumerate(summen):
-        plt.text(i, value + 1, f'{value:.2f}', ha='center', va='bottom')
-
-    plt.savefig(path, format='png', dpi=300, bbox_inches='tight')
-    plt.show()
-
-def plotPiePercent(vec, filename: str):
-
-    path = "../data/" + filename + ".png"
-    labels = [f"{i}/{10} EEs" if vec[i] > 0 else "" for i in range(len(vec))]
-
-    def func(pct):
-        if pct > 0:
-            return f'{pct:.0f}%'
-        return ''
-    
-    plt.figure(figsize=(10, 6))
-    plt.pie(vec, labels = labels, autopct=lambda pct: func(pct), startangle=90, colors=plt.cm.Paired.colors, wedgeprops={'width': 0.3}, pctdistance=0.85)
-    plt.title('Anzahl von Viertelstunden mit [%] Anteil von Erneuerbaren')
-    plt.savefig(path, format='png', dpi=300, bbox_inches='tight')
-    
-    plt.show()
-
-def plotPieChart(df, filename: str):
-    path = "../data/" + filename + ".png"
+#plots piechart of energyproduction
+def plot_pie_prod(df, filename: str):
 
     erneuerbare_spalten = ['Biomasse', 'Wasserkraft', 'Wind Offshore', 'Wind Onshore', 'Photovoltaik', 'Sonstige Erneuerbare']
     konventionelle_spalten = ['Kernenergie', 'Braunkohle', 'Steinkohle', 'Erdgas', 'Sonstige Konventionelle']
-    
     erneuerbare_summe = df[erneuerbare_spalten].sum().sum()/(1e+6)
     konventionelle_summe = df[konventionelle_spalten].sum().sum()/(1e+6)
-
     labels = [f'Erneuerbare Energie ({erneuerbare_summe:.2f} TWh)', f'Konventionelle Energie ({konventionelle_summe:.2f} TWh)']
-    values = [erneuerbare_summe, konventionelle_summe]
+    data = [erneuerbare_summe, konventionelle_summe]
 
-    plt.figure(figsize=(10, 6))
-    plt.pie(values, labels=labels, autopct='%1.2f%%', startangle=90)
-    plt.title(filename)
-    plt.savefig(path, format='png', dpi=300)
+    plot_pie(data, labels, filename)
 
-    plt.show()
-
-def plotPieChartVer(df, df2, filename: str):
-    path = "../data/" + filename + ".png"
-
+#plots piechart of energyusage
+def plot_pie_usage(df, df2, filename: str):
     
     erneuerbare_summe = (df['Gesamt'].sum().sum()-df['Residuallast'].sum().sum())/(1e+6)
     erneuerbare_summe_res = (df2['Biomasse'].sum().sum()+ df2['Wasserkraft'].sum().sum() + df2['Sonstige Erneuerbare'].sum().sum())/(1e+6)
     konventionelle_summe = df['Residuallast'].sum().sum()/(1e+6)- erneuerbare_summe_res
-
     labels = [f'Photovoltaik und Windkraft ({erneuerbare_summe:.2f} TWh)', f'Biomasse, Wasserkraft, Sonst. EEs ({erneuerbare_summe_res:.2f} TWh)', f'Konventionelle Energie ({konventionelle_summe:.2f} TWh)']
-    values = [erneuerbare_summe, erneuerbare_summe_res ,konventionelle_summe]
+    data = [erneuerbare_summe, erneuerbare_summe_res ,konventionelle_summe]
 
-    plt.figure(figsize=(10, 6))
-    plt.pie(values, labels=labels, autopct='%1.2f%%', startangle=90)
-    plt.title(filename)
-    plt.savefig(path, format='png', dpi=300)
+    plot_pie(data, labels, filename)
 
-    plt.show()
+#plots piechart of renewables
+def plot_pie_rene(df, filename: str):
 
-def plotPieErzeugerNeu(df, filename: str):
+    erzeuger_spalten = ['Biomasse', 'Wasserkraft', 'Wind Offshore', 'Wind Onshore', 'Photovoltaik', 'Sonstige Erneuerbare']
+    data = df[erzeuger_spalten].sum() / 1e+6
+    labels = [f'{name} ({value:.2f} TWh)' for name, value in zip(erzeuger_spalten, data)]
 
-    path = "../data/" + filename + ".png"
-    erzeuger_spalten = [
-        'Biomasse', 'Wasserkraft', 'Wind Offshore', 'Wind Onshore',
-        'Photovoltaik', 'Sonstige Erneuerbare'
-    ]
+    plot_pie(data, labels, filename)
 
-    summen = df[erzeuger_spalten].sum() / 1e+6
+#plots piechart of conventionals
+def plot_pie_conv(df, filename: str):
 
-    labels = [f'{name} ({value:.2f} TWh)' for name, value in zip(erzeuger_spalten, summen)]
-
-    plt.figure(figsize=(10, 6))
-    plt.pie(
-        summen,
-        labels=labels,
-        autopct='%1.2f%%',
-        startangle=140,
-        colors=plt.cm.Paired.colors
-    )
-    plt.title(filename)
-    plt.savefig(path, format='png', dpi=300)
-    plt.show()
-
-def plotPieErzeugerKonv(df, filename: str):
-
-    path = "../data/" + filename + ".png"
     konventionelle_spalten = ['Kernenergie', 'Braunkohle', 'Steinkohle', 'Erdgas', 'Sonstige Konventionelle']
+    data = df[konventionelle_spalten].sum() / 1e+6
+    labels = [f'{name} ({value:.2f} TWh)' for name, value in zip(konventionelle_spalten, data)]
 
-    summen = df[konventionelle_spalten].sum() / 1e+6
+    plot_pie(data, labels, filename)
 
-    labels = [f'{name} ({value:.2f} TWh)' for name, value in zip(konventionelle_spalten, summen)]
+
+#visualization of piecharts
+def plot_pie(data, labels, filename: str):
+
+    path = "../data/" + filename + ".png"
 
     plt.figure(figsize=(10, 6))
     plt.pie(
-        summen,
+        data,
         labels=labels,
         autopct='%1.2f%%',
-        startangle=140,
+        startangle=90,
         colors=plt.cm.Paired.colors
     )
     plt.title(filename)
     plt.savefig(path, format='png', dpi=300)
     plt.show()
 
+#visualization of columncharts
+def plotBalken(data, ylabel, xlabel, filename: str):
+
+    path = "../data/" + filename + ".png"
+
+    plt.figure(figsize=(10, 6))
+    data.plot(kind='bar', color='skyblue')
+    plt.title(filename)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.xticks(rotation=15)
+
+    for i, value in enumerate(data):
+        plt.text(i, value + 1, f'{value:.2f}', ha='center', va='bottom')
+
+    plt.savefig(path, format='png', dpi=300)
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -11,19 +11,20 @@ def main():
     con22 = data.read_SMARD("Realisierter_Stromverbrauch_202201010000_202301010000_Viertelstunde.csv", False)
     con23 = data.read_SMARD("Realisierter_Stromverbrauch_202301010000_202401010000_Viertelstunde.csv", False)
     
-    
-    #print(gen21)
-    #heatmaps(df)
-    #histogramErzeuger(gen21, '2021')
-    #histogramErzeuger(gen22, '2022')
-    #histogramErzeuger(gen23, '2023')
-    histogramPercent(con21, gen21, '2021')
-    histogramPercent(con22, gen22, '2022')
-    histogramPercent(con23, gen23, '2023')
-    graphics.plotBalken(gen21, 'Alle Energieträger 2021')
-    graphics.plotBalken(gen22, 'Alle Energieträger 2022')
-    graphics.plotBalken(gen23, 'Alle Energieträger 2023')
-    #data.appendCSV(gen21, gen22, gen23, con21, con22, con23)
+    print(gen21)
+    graphics.plotHeatmap(gen22, 'Wind Onshore', 'Monat', 'Uhrzeit', 'Heatmap % ')
+    #plot_data(gen21, con21, '2021')
+
+
+def plot_data(dfe, dfv, time):
+    graphics.plot_pie_conv(dfe, 'Anteilige Erzeugung Konventioneller '+ time)
+    graphics.plot_pie_rene(dfe, 'Anteilige Erzeugung Erneuerbarer '+ time)
+    graphics.plot_pie_usage(dfv, dfe, 'Erneuerbare vs. Konventionelle Energie Verbrauch '+ time)
+    graphics.plot_pie_prod(dfe, 'Erneuerbare vs. Konventionelle Energie '+ time)
+    graphics.plot_balk_rene(dfe, 'Anteil der einzelnen Erzeuger an den erneuerbaren Energien '+ time)
+    graphics.plot_balk_all(dfe, 'Erzeugte Leistung der einzelnen Energieträger ' + time)
+    graphics.plotHistogramPercent(dfe, 'Histogramm Abdeckung der Viertelstunden ' +time)
+    graphics.plotHeatmap(dfe, 'Anteil Erneuerbar [%]', 'Monat', 'Tag', 'Heatmap % ' + time)
 
 
 def heatmaps(df):
@@ -33,17 +34,9 @@ def histogramPercent(df, df2, time: str):
     df = data.addPercentageRenewableLast(df, df2)
     vec = data.countPercentageRenewable(df)
     #vec = data.countPercentageRenewableExclude(df)
-    graphics.plotHistogramPercent2(df, 'Histogramm Abdeckung der Viertelstunden ' +time)
+    graphics.plotHistogramPercent(df, 'Histogramm Abdeckung der Viertelstunden ' +time)
     graphics.plotHistogramPercent(vec, 'Anzahl der Viertelstunden mit prozentualer Abdeckung für ' +time)
-    #graphics.plotHeatmap(df, 'Anteil Erneuerbar [%]', 'Monat', 'Tag', 'Heatmap % ' + time)
-    graphics.plotPieChartVer(df, df2, 'Erneuerbare vs. Konventionelle Energie Verbrauch '+time)
-    #graphics.plotPiePercent(vec, "Pie_chart2021")
-
-def histogramErzeuger(df, time: str):
-    graphics.plotHistogramErzeuger(df, 'Anteil der einzelnen Erzeuger an den erneuerbaren Energien '+ time)
-    graphics.plotPieChart(df, 'Erneuerbare vs. Konventionelle Energie '+ time)
-    graphics.plotPieErzeugerNeu(df, 'Anteilige Erzeugung Erneuerbarer '+ time)
-    graphics.plotPieErzeugerKonv(df, 'Anteilige Erzeugung Konventioneller '+ time)
+    graphics.plotHeatmap(df, 'Anteil Erneuerbar [%]', 'Monat', 'Tag', 'Heatmap % ' + time)
 
 
 if __name__ == "__main__":
