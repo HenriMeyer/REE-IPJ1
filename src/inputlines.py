d591cda67923f772da7f927e1386cef6e8df408b
HenriMeyer <meyer_henri.1@web.de>
2025-01-21 15:34:20 +0100

    further change

--- src/inputlines.py
+++ src/inputlines.py
@@ -1,8 +1,9 @@
-import os
 
-filename: str = "inputlines.txt"
+FILENAME = "inputlines.txt"
+HOWOFTEN = 5
 
 def main():
+    # start values
     PV = 0
     radiation = 0
     windOnshore = 0
@@ -22,26 +23,36 @@ def main():
     batteryLoad = 0
     
     simulationList = ["PV", "OnShore", "Offshore", "Speicher"]
-    
 
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write("own\nvalues\n")
+    with open(FILENAME, 'w', encoding='utf-8') as f:
+        for i in range(HOWOFTEN):
+            szenarioName = "Test" + str(i)
+            f.write("own\nvalues\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\n100000\nyes\n" + szenarioName + "\ny\n")
+        f.write("visualize\nall\nquit\n")
+        # f.write("own\nvalues\n")
         # for name in simulationList:
         #     if name == "PV":
-                
+        #         for i in range(HOWOFTEN):
+                    
         #     if name == "OnShore":
-                
+        #         for i in range(HOWOFTEN):
         #     if name == "Offshore":
-                
+        #         for i in range(HOWOFTEN):
         #     if name == "Speicher":
-                
+        #         for i in range(HOWOFTEN):
         #     else:
         #         print("Error")
         #         break
     
-    
-    
-    
-    
+def writeToFile(list: list()) -> str:
+    if list:
+        stringToWrite = None
+        # with open(FILENAME, 'w', encoding='utf-8') as f:
+        #     for line in list:
+        #         f.write(line)
+        return stringToWrite
+    else:
+        return "quit"
+
 if __name__ == "__main__":
     main()
